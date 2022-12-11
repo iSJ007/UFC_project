20703b05a5f62a8357f89753f4d69be196aef8a1
iSJ007 <66101877+iSJ007@users.noreply.github.com>
2022-12-11 10:06:01 -0800

    Update fantF.php

--- UFC_project/fantF.php
+++ UFC_project/fantF.php
@@ -18,7 +18,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>UFCFantasyFight</title>
     <link href="style/styles4.css" rel="stylesheet" >
 </head>
 
